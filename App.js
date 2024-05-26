ccee67e4faf7b25986abc8ff24e78f8c0a110d5d
Nayaohenewa <noofosu_asamani@st.ug.edu.gh>
2024-05-26 21:31:17 +0000

    Designing the page
    using javascript to design the page

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'React';
+import { Stylesheet, Text, View } from 'react-native';
+
+export default function App() {
+return (
+    <View style={Stylesheet.container}>
+        <Text style={Stylesheet.text}>
+            My name is Naya <text style={Stylesheet.boldText}>Naya</text>
+        </Text>
+    </View>
+);
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 50,   
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+
+
+    }
+)
